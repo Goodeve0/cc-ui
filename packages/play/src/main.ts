9c904e498a83b2a0c01781cab9deaa91b52d788f
c <2655774991@qq.com>
2024-10-11 18:46:07 +0800

    fix: remove redundant code

--- packages/play/src/main.ts
+++ packages/play/src/main.ts
@@ -1,10 +1,7 @@
 import { createApp } from "vue";
 import App from "./app.vue";
-import CUI from "c-ui/components";
 import { CButton } from "c-ui";
 import { CTree } from "c-ui";
-console.log(CUI);
-console.log(CTree);
 
 const app = createApp(App);
 app.use(CButton as any);
