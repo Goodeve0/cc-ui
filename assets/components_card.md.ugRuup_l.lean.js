7f896559f58ee33df118f633ec2578220e369122
Goodeve0 <Goodeve0@users.noreply.github.com>
2025-02-09 12:19:49 +0000

    deploy: f3630dcf26901e3c0778ccaf9cacd42fafd2552a

---
+++ assets/components_card.md.ugRuup_l.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./chunks/framework.CFQJLYDs.js";const i=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"components/card.md","filePath":"components/card.md","lastUpdated":1734108334000}'),o={name:"components/card.md"};function c(r,n,s,d,p,m){return a(),t("div")}const f=e(o,[["render",c]]);export{i as __pageData,f as default};
